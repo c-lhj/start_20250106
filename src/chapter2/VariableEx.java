26355fa8cef3a2af3971b3e66312309d7e63013b
hj98340 <hj98340@gmail.com>
2025-01-22 22:18:44 +0900

    2025_0122
    변수의 선언과 기본 자료형

---
+++ src/chapter2/VariableEx.java
@@ -0,0 +1,27 @@
+package chapter2;
+
+public class VariableEx {
+
+	public static void main(String[] args) {
+		
+		int num; // 노란줄은 경고의 의미. 여기서는 한 번도 사용하지 않아서 나오는 경고.		
+		num = 10; // l value, r value
+		
+		// 변수를 선언하고 처음으로 값을 넣는 것을 '초기화' 라고 한다.
+		
+		System.out.println(num);
+		
+		int level = 20;
+		
+		// 선언과 동시에 초기화를 할 수도 있다.
+		
+		System.out.println(level);
+		
+		int ns;
+		int numberOfStudent;
+		
+		// 변수의 이름을 명명할 때, 의미있게 작성하여 누구라도 이해하기 쉽도록 하는게 좋다.
+		
+	}
+
+}
